75eabb6e1281e90a21d8ab1ad774cf719199a9e0
mrodr324 <mrodr324@ucr.edu>
2023-08-22 17:09:47 -0700

    Adding files and updates

--- ConvertToTxtSimple.sh
+++ ConvertToTxtSimple.sh
@@ -1,7 +1,7 @@
-
+foldername="Aug222023SourceUCRStrip4V_2"
 for i in {00000001..00000001}
 	do
 		new=$(echo $i | sed 's/^0*//')
-		ddump -i -n 400 rcdaq-$i-0000.evt > $new.txt
+		ddump -i -n 10000 ../SiPMData/$foldername/rcdaq-$i-0000.evt > ../SiPMData/$foldername/$new.txt
 		echo $i
 	done
